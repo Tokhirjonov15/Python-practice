1b567896a45f1eacea6cf0bc335de3e2a16bf4e6
Alex <mntokhirjonov@gmail.com>
2026-04-05 13:47:50 +0900

    feat: learn Python's opertaors

---
+++ opercon.py
@@ -0,0 +1,22 @@
+''' OPERATORS & CONDITIONS
+    (1) Operators
+    (2) Condition
+    (3) Logical Operators
+'''
+
+print("===== Operators =====")
+
+a = 19
+b = 5
+
+print(a / b)
+result = a // b  # bo'lingandagi butun son
+left = a % b    # bo'lingandagi qoldiq son
+print(f"the result: {result} and left: {left}")
+
+# a = a + 100
+a += 100
+print("a:", a)
+
+print("Bning kvadrati:", b**2)
+print("Bning kubi:", b**3)
